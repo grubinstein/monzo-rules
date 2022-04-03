588ce0e16717b7ae634e623089193323d58b127b
Gabriel Rubinstein <gabrielrubinstein1@googleamil.com>
2022-04-03 23:36:43 +0100

    Minor refactoring

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import express from "express";
 import sequelize from "./db/sequelize.js";
-import { authorize, authReturn } from "./monzo/monzoClient.js";
-import webhookService from "./monzo/webhookService.js";
+import monzoClient from "./monzo/monzoClientComposer.js";
+import webhookService from "./monzo/webhookHandlerComposer.js";
 import bodyParser from "body-parser";
 
-sequelize.sync();
+await sequelize.sync();
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/auth", authorize);
+app.get("/auth", monzoClient.authorize);
 
-app.get("/authorizereturn", authReturn);
+app.get("/authorizereturn", monzoClient.authReturn);
 
 app.get("/", (req, res) => res.send("Hello"));
 
