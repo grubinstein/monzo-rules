baa7c9aad47cb7219a7dd402d9d6bfe832b57241
Gabriel Rubinstein <gabrielrubinstein1@googleamil.com>
2022-04-03 23:34:12 +0100

    Fix SQLITE_BUSY problem by changing transactionType to EXCLUSIVE

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -9,6 +9,15 @@ const sequelize = new Sequelize(database, username, password, {
   dialect,
   storage,
   logging,
+  transactionType: "EXCLUSIVE",
+  retry: {
+    max: 10,
+    match: [
+      "SQLITE_BUSY",
+      "SQLITE_BUSY: database is locked",
+      "Error: SQLITE_BUSY: database is locked",
+    ],
+  },
 });
 
 export default sequelize;
