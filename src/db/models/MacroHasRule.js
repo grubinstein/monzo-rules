111141cf62b03df7550b75808de2349602c7ed19
Gabriel Rubinstein <gabrielrubinstein1@googleamil.com>
2022-04-03 23:35:29 +0100

    Move model relations to separate file

--- src/db/models/MacroHasRule.js
+++
@@ -1,7 +0,0 @@
-import Rule from "./Rule.js";
-import Macro from "./Macro.js";
-
-Rule.belongsToMany(Macro, { through: "RuleHasMacro" });
-Macro.belongsToMany(Rule, { through: "RuleHasMacro" });
-
-export default { Rule, Macro };
