80c4779290f2563eb80a02325425d05f311d90b2
Gabriel Rubinstein <gabrielrubinstein1@googleamil.com>
2022-04-03 23:36:07 +0100

    Add user object to transaction and implement DI

--- src/macros/macro.js
+++ src/macros/macro.js
@@ -1,37 +1,40 @@
-import workers from "./workers.js";
-
-const runMacros = async (macros, transaction) => {
-  if (!macros || !macros.length) {
-    return;
-  }
-  for (let i = 0; i < macros.length; i++) {
-    const macro = macros[i];
-    await runMacro(macro, transaction);
-  }
-};
-
-const runMacro = async (macro, transaction) => {
-  if (!macro.tasks || !macro.tasks.length) {
-    return;
-  }
-  const tasks = [...macro.tasks];
-  const variables = {
-    transactionAmount: transaction.amount,
-    transactionId: transaction.id,
-    macroName: macro.name,
+const createRunMacros = (workers, colour) => {
+  const runMacros = async (macros, transaction) => {
+    if (!macros || !macros.length) {
+      return;
+    }
+    for (let i = 0; i < macros.length; i++) {
+      const macro = macros[i];
+      await runMacro(macro, transaction);
+    }
   };
-  await performNextTask(variables, tasks);
+
+  const runMacro = async (macro, transaction) => {
+    if (!macro.tasks || !macro.tasks.length) {
+      return;
+    }
+    const tasks = [...macro.tasks];
+    const variables = {
+      user: transaction.user,
+      transactionAmount: transaction.amount,
+      transactionId: transaction.id,
+      macroName: macro.name,
+    };
+    await performNextTask(variables, tasks);
+  };
+
+  const performNextTask = async (variables, tasks) => {
+    const task = tasks.shift();
+    const worker = workers[task.type];
+    const newVariables = await worker(variables, task);
+    if (tasks.length) {
+      await performNextTask(newVariables, tasks);
+    } else {
+      colour.log("Completed running tasks.", variables.transactionId);
+    }
+  };
+
+  return runMacros;
 };
 
-const performNextTask = async (variables, tasks) => {
-  const task = tasks.shift();
-  const worker = workers[task.type];
-  const newVariables = await worker(variables, task);
-  if (tasks.length) {
-    await performNextTask(newVariables, tasks);
-  } else {
-    console.log("Completed running tasks.");
-  }
-};
-
-export default runMacros;
+export default createRunMacros;
