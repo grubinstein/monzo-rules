1ff13e3e4e799cf4159df60aa29a36d80a5c240f
Gabriel Rubinstein <gabrielrubinstein1@googleamil.com>
2022-04-03 23:33:09 +0100

    Add adapter for sequelize

--- src/db/dbAdapter.js
+++ src/db/dbAdapter.js
@@ -1,6 +1,57 @@
-import Request from "./models/Request.js";
-import MacroHasRule from "./models/MacroHasRule.js";
-const { Rule, Macro } = MacroHasRule;
+import Models from "./relations.js";
+const { Rule, Macro, User, Request } = Models;
+
+export const storeUserAccessData = async ({
+  access_token: accessToken,
+  refresh_token: refreshToken,
+  user_id: userId,
+}) => {
+  const email = "gabrielrubinstein1@googlemail.com";
+  const user = await User.findOne({ where: { email } });
+  if (!user) {
+    await User.create({
+      email,
+      accessToken,
+      refreshToken,
+      userId,
+    });
+    return;
+  }
+  await user.update({ accessToken, refreshToken });
+  return user;
+};
+
+export const getUserByAccountId = async (accountId) => {
+  const user = await User.findOne({ where: { accountId } });
+  return user;
+};
+
+export const getUserByEmail = async (email) => {
+  const user = await User.findOne({ where: { email } });
+  if (!user) {
+    throw new Error("User not found");
+  }
+  return user;
+};
+
+export const getAccessToken = async (email) => {
+  const user = await getUserByEmail(email);
+  return user.accessToken;
+};
+
+export const getRefreshToken = async (passedUser) => {
+  const user = await User.findByPk(passedUser.id);
+  return user.refreshToken;
+};
+
+export const setAccountId = async (user, accountId) => {
+  await User.update({ accountId }, { where: { id: user.id } });
+};
+
+export const getUserIdByEmail = async (email) => {
+  const user = await getUserByEmail(email);
+  return user.id;
+};
 
 export const requestExists = async (transaction) => {
   const { id: transactionId, callType } = transaction;
@@ -20,9 +71,23 @@ export const addRequest = async (transaction) => {
   });
 };
 
+export const addRequestIfNew = async (transaction) => {
+  const { id: transactionId, dedupe_id, callType } = transaction;
+  const [_, created] = await Request.findOrCreate({
+    where: { transactionId, callType },
+    defaults: {
+      transactionId,
+      dedupe_id,
+      callType,
+      transaction,
+    },
+  });
+  return created;
+};
+
 export const addRule = async (ruleObject) => {
-  const { name, filters, macros } = ruleObject;
-  const rule = await Rule.create({ name, filters });
+  const { name, filters, UserId, macros } = ruleObject;
+  const rule = await Rule.create({ name, filters, UserId });
   if (macros && macros.length) {
     for (let i = 0; i < macros.length; i++) {
       let macro = macros[i];
