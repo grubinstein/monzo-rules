a4a03243ec39229db7a57389d82e18ee98bdfb40
Gabriel Rubinstein <gabrielrubinstein1@googleamil.com>
2022-04-03 23:35:36 +0100

    Additional logging

--- src/errors/axiosErrors.js
+++ src/errors/axiosErrors.js
@@ -1,13 +1,17 @@
 const handleAxiosErrors = (error) => {
+  console.log("ERROR!");
   if (error.response) {
+    console.log("Server responded");
     // Request made and server responded
     console.log(error.response.data);
     console.log(error.response.status);
     console.log(error.response.headers);
   } else if (error.request) {
+    console.log("No response");
     // The request was made but no response was received
     console.log(error.request);
   } else {
+    console.log("Error setting up request");
     // Something happened in setting up the request that triggered an Error
     console.log("Error", error.message);
   }
