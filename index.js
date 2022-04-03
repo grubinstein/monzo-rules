70e520bb8f23760590d0ebbb41037b99bed0357c
Gabriel Rubinstein <gabrielrubinstein1@googleamil.com>
2022-04-03 23:26:03 +0100

    Implement https

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-import { addTestRules } from "./testRules.js";
-import { registerWebHook } from "./src/monzo/monzoClient.js";
+import https from "https";
+import fs from "fs";
+import { addDevRules } from "./test/devRules.js";
 import app from "./src/app.js";
 
 app.listen(3000, () => {
-  console.log("App listening on port 3000");
+  console.log("http server listening on port 3000");
 });
 
-await registerWebHook();
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(8000, () => {
+    console.log("https server listening on port 8000");
+  });
 
 if (process.env.NODE_ENV == "development") {
-  addTestRules();
+  //addDevRules();
 }
