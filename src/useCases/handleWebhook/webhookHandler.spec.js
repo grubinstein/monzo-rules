770f424dc5529f629283f6e090b98ebd4c5a36ba
Gabriel Rubinstein <gabrielrubinstein1@googleamil.com>
2022-04-04 15:31:14 +0100

    Unit test webhookHandler

---
+++ src/useCases/handleWebhook/webhookHandler.spec.js
@@ -0,0 +1,103 @@
+import { jest } from "@jest/globals";
+import webhookComposer from "../../../test/mockWebhookComposer.js";
+import mockWebhookRequest from "../../../test/mockWebhookRequest.js";
+
+const processTransaction = jest.fn();
+const logger = { log: jest.fn() };
+const db = {
+  addRequestIfNew: jest.fn(),
+  getUserByAccountId: jest.fn(),
+};
+
+const webhookHandler = webhookComposer({
+  processTransaction,
+  logger,
+  db,
+}).webhookHandler;
+
+const res = (() => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  res.send = jest.fn().mockReturnValue(res);
+  return res;
+})();
+
+jest.useFakeTimers();
+
+beforeEach(() => {
+  jest.resetAllMocks();
+  jest.clearAllMocks();
+  ["status", "json", "send"].forEach((k) => {
+    res[k].mockReturnValue(res);
+  });
+});
+
+describe("webhook handler", () => {
+  it("passes transaction to db to check if new and store", async () => {
+    await webhookHandler(mockWebhookRequest, res);
+    expect(db.addRequestIfNew).toHaveBeenCalled();
+  });
+  it("adds call type to transaction before passing to db", async () => {
+    await webhookHandler(mockWebhookRequest, res);
+    expect(db.addRequestIfNew.mock.calls[0][0].callType).toBe(
+      "transaction.created"
+    );
+  });
+  it("it logs for every webhook call", async () => {
+    await webhookHandler(mockWebhookRequest, res);
+    expect(logger.log).toHaveBeenCalled();
+  });
+  it("it logs Handling message for new transactions/updates", async () => {
+    db.addRequestIfNew.mockReturnValue(true);
+    await webhookHandler(mockWebhookRequest, res);
+    const {
+      type,
+      data: { description, id },
+    } = mockWebhookRequest.body;
+    expect(logger.log.mock.calls[0][0]).toBe(
+      `Handling: ${description} ${id} ${type}`
+    );
+  });
+  it("it logs Repeat message for duplicate transactions/updates", async () => {
+    db.addRequestIfNew.mockReturnValue(false);
+    await webhookHandler(mockWebhookRequest, res);
+    const {
+      type,
+      data: { description, id },
+    } = mockWebhookRequest.body;
+    expect(logger.log.mock.calls[0][0]).toBe(
+      `Repeat: ${description} ${id} ${type}`
+    );
+  });
+  it("passes id to logger", async () => {
+    db.addRequestIfNew.mockReturnValue(false);
+    await webhookHandler(mockWebhookRequest, res);
+    expect(logger.log.mock.calls[0][1]).toBe(mockWebhookRequest.body.data.id);
+  });
+  it("calls processTransaction for new transactions/updates", async () => {
+    db.addRequestIfNew.mockReturnValue(true);
+    await webhookHandler(mockWebhookRequest, res);
+    expect(processTransaction).toHaveBeenCalled();
+  });
+  it("passes transaction to processTransaction", async () => {
+    db.addRequestIfNew.mockReturnValue(true);
+    await webhookHandler(mockWebhookRequest, res);
+    expect(Object.keys(processTransaction.mock.calls[0][0])).toEqual(
+      expect.arrayContaining(Object.keys(mockWebhookRequest.body.data))
+    );
+  });
+  it("adds user to transaction before passing to processTransaction", async () => {
+    db.addRequestIfNew.mockReturnValue(true);
+    const mockUser = { id: 1, email: "user@mail.com" };
+    db.getUserByAccountId.mockReturnValue(mockUser);
+    await webhookHandler(mockWebhookRequest, res);
+    expect(processTransaction.mock.calls[0][0].user).toEqual(mockUser);
+  });
+  it("returns 200", async () => {
+    await webhookHandler(mockWebhookRequest, res);
+    expect(res.status).toHaveBeenCalled();
+    expect(res.status.mock.calls[0][0]).toBe(200);
+    expect(res.send).toHaveBeenCalled();
+  });
+});
